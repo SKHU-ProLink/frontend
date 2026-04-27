ed5978626228899f88a7838c5899d0e33e578a5a
Han, Siyeon <hsiyeon@icloud.com>
2026-04-28 08:33:01 +0900

    feat: 듣기 버튼 생성

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,7 @@
-import FlashCard from '@/components/flash-card';
 import { StyleSheet, View } from 'react-native';
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <FlashCard front={'ㅇㅇ'} back={{
-        meaning: "사과",
-        partOfSpeech: "명사",
-        pronunciation: "/æp.əl/"
-    }} />
     </View>
   );
 }
@@ -18,21 +12,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     padding: 24,
     gap: 10
-  },
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
   },
 });
