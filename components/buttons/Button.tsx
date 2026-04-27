9c70116fced4e095866f6912f506855fa21153d4
Han, Siyeon <hsiyeon@icloud.com>
2026-04-28 08:44:41 +0900

    feat: 말해보기 버튼 생성

--- components/buttons/Button.tsx
+++ components/buttons/Button.tsx
@@ -1,14 +1,23 @@
 import { Pressable, StyleSheet, Text } from "react-native";
+import VoiceIcon from '../../assets/images/voice.svg';
 import { colors } from "../../constants/colors";
 interface ButtonProps {
     title: string;
     onPress: () => void;
-    variant?: 'primary' | 'secondary' | 'disabled';
+    variant?: 'primary' | 'secondary' | 'disabled' | 'speaking';
 }   
 const Button = ({ title, onPress, variant = 'primary' }: ButtonProps) => {
     return (
-        <Pressable onPress={onPress} style={[styles.button, variant === 'secondary' && styles.secondaryButton, variant === 'disabled' && styles.disabledButton]}>
-            <Text style={styles.buttonText}>{title}</Text>
+        <Pressable onPress={onPress} style={[styles.button, variant === 'secondary' && styles.secondaryButton, variant === 'disabled' && styles.disabledButton, variant === "speaking" && styles.speakingButton] }>
+            {variant !== "speaking" ?
+                (<Text style={styles.buttonText}>{title}</Text>) :
+                (
+                    <>
+                        <VoiceIcon width={24} height={24} />
+                        <Text style={[styles.buttonText, { color: colors.primary[500] }]}>{title}</Text>
+                    </>
+                )
+            }
         </Pressable>
     );
 
@@ -35,6 +44,13 @@ const styles = StyleSheet.create({
     disabledButton: {
         backgroundColor: colors.grayscale[400],
         opacity: 0.5
+    },
+    speakingButton: {
+        borderWidth: 1.5,
+        borderColor: colors.primary[500],
+        backgroundColor: colors.primary[50],
+        flexDirection: 'row',
+        gap: 6
     }
 }); 
 
