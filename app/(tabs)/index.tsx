b8560f2a481c912114416fe03fa61caa7abd0b14
Han, Siyeon <hsiyeon@icloud.com>
2026-04-30 16:56:28 +0900

    feat: 결과 바텀 시트 컴포넌트 제작

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,7 @@
-import SentenceCard from '@/components/quiz/sentence-card';
 import { StyleSheet, View } from 'react-native';
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <SentenceCard title={'안머겅'} onPress={function (): void {
-        throw new Error('Function not implemented.');
-      } } />
     </View>
   );
 }
@@ -14,7 +10,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    padding: 24,
     gap: 10
   },
 });
