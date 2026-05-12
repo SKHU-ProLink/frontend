e255f14daada0dcbf0607475aabd3836fa4fedd0
Han, Siyeon <hsiyeon@icloud.com>
2026-05-12 17:44:52 +0900

    feat: icon-text-pill 컴포넌트

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,7 +1,11 @@
+import IconTextPill from '@/components/icon-text-pill';
+import StoreIcon from '@/assets/images/store.svg';
 import { StyleSheet, View } from 'react-native';
+
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
+      <IconTextPill Icon={StoreIcon} text="상점" />
     </View>
   );
 }
@@ -10,6 +14,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    gap: 10
+    gap: 10,
+    paddingTop: 80,
+    paddingHorizontal: 24,
   },
 });
